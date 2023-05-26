e7ab183db6020e5fd3242f2d58af9ed06df4060d
22017143 <22017143@10.172.166.154>
2023-05-26 14:08:01 +0800

    First push to P00 of GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator {
+
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract (int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply (int a, int b) {
+		return a * b;
+	}
+	
+	public int divide (int a, int b) {
+		return a / b;
+	}
+}
+
+
+//	Github
+//	https://github.com/22017143-Leong-YunHou/P00.git
